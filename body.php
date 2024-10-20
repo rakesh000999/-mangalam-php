5d7b74297c80e6a5cc3bd839c91f2da40d384268
rakesh000999 <rakeshjoshi000999@gmail.com>
2024-10-20 12:57:38 +0545

    comment feature

--- body.php
+++ body.php
@@ -2,60 +2,80 @@
 include 'connection.php';
 include 'nav.php';
 
-$fetchSql = "SELECT post_id, username, title, content, created_at FROM users INNER JOIN posts ON users.user_id = posts.user_id";
+// Fetch posts along with user information
+$fetchSql = "SELECT post_id, username, title, content, created_at 
+             FROM users 
+             INNER JOIN posts ON users.user_id = posts.user_id
+             ORDER BY created_at DESC";
 $fetchResult = mysqli_query($conn, $fetchSql);
 
+// Fetch comment counts grouped by post_id
+$selectComment = "SELECT post_id, COUNT(comment_id) AS comment_count 
+                  FROM comments 
+                  GROUP BY post_id";
+$selectCommentResult = mysqli_query($conn, $selectComment);
+
+// Store comment counts in an associative array
+$commentCounts = [];
+while ($commentRow = mysqli_fetch_assoc($selectCommentResult)) {
+    $commentCounts[$commentRow['post_id']] = $commentRow['comment_count'];
+}
 ?>
+
 <link rel="stylesheet" href="style.css">
 
-
 <main class="row container-lg mx-auto ">
-    <div class=" border-end col-lg-8">
+    <div class="border-end col-lg-8">
         <?php
         while ($result = mysqli_fetch_assoc($fetchResult)) {
+            $postId = $result['post_id'];
+            // Check if this post has comments, otherwise default to 0
+            $commentCount = isset($commentCounts[$postId]) ? $commentCounts[$postId] : 0;
         ?>
             <div class="card d-flex m-2 p-2">
                 <div>
                     <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
-                        alt=""
-                        class="image">
-                    <span><a href="viewOthersProfile.php" class="text-decoration-none text-dark"><?php echo $result['username'] ?></a></span>
+                        alt="Profile Image" class="image">
+                    <span>
+                        <a href="viewOthersProfile.php" class="text-decoration-none text-dark">
+                            <?php echo $result['username']; ?>
+                        </a>
+                    </span>
                 </div>
-                <a href="read-blog.php?id=<?php echo $result['post_id'] ?>"
-                    class="text-decoration-none text-dark">
+                <a href="read-blog.php?id=<?php echo $postId; ?>" class="text-decoration-none text-dark">
                     <div class="d-flex">
                         <div class="w-75">
-                            <h2 class="fw-bolder"><?php echo $result['title'] ?></h2>
-                            <h4><?php echo $result['content'] ?></h4>
+                            <h2 class="fw-bolder"><?php echo $result['title']; ?></h2>
+                            <h4><?php echo $result['content']; ?></h4>
                         </div>
                         <div class="w-25 object-fit-fill">
-                            <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-beach-free-image-after-sunset-sky-free-photo.jpeg?w=600&quality=80" alt="image" class="w-100">
+                            <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-beach-free-image-after-sunset-sky-free-photo.jpeg?w=600&quality=80"
+                                alt="Blog Image" class="w-100">
                         </div>
                     </div>
                     <div class="d-flex gap-4">
-                        <div><?php echo $result['created_at'] ?></div>
-                        <div>👍1</div>
-                        <div>💬 2</div>
+                        <div><?php echo $result['created_at']; ?></div>
+                        <div>👍 0</div>
+                        <div><?php echo ($commentCount === 0) ? '' : "💬" . $commentCount; ?></div>
+
                     </div>
                 </a>
             </div>
         <?php
         }
         ?>
-
     </div>
 
     <div class="col-lg-4">
-        <?php foreach (range(1, 5) as $i) {
-        ?>
-            <div class="">
+        <?php foreach (range(1, 5) as $i) { ?>
+            <div>
                 <div>
-                    <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="" class="image">
-                    <span>Rakesh Joshi </span>
+                    <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+                        alt="Profile Image" class="image">
+                    <span>Rakesh Joshi</span>
                 </div>
                 <p class="h-4">Lorem ipsum dolor sit amet consectetur.</p>
             </div>
-        <?php
-        } ?>
+        <?php } ?>
     </div>
 </main>
--- read-blog.php
+++ read-blog.php
@@ -2,6 +2,8 @@
 include 'connection.php';
 include 'nav.php';
 
+$user_id = $_SESSION['user_id'];
+
 $getBlogId = $_GET['id'];
 
 $fetchSql = "SELECT * FROM posts WHERE post_id = '$getBlogId'";
@@ -9,9 +11,40 @@ $fetchResult = mysqli_query($conn, $fetchSql);
 
 $data = mysqli_fetch_assoc($fetchResult);
 
+// comment
+$selectUser = "SELECT * FROM users WHERE user_id = $user_id";
+$selectUserResult = mysqli_query($conn, $selectUser);
+
+$userData = mysqli_fetch_assoc($selectUserResult);
+
+$comment = '';
+$error = [];
+
+$isValid = true;
+
+if (isset($_POST['submit'])) {
+    if (isset($_POST['comment-text']) && !empty($_POST['comment-text']) && trim($_POST['comment-text'])) {
+        $comment = $_POST['comment-text'];
+    } else {
+        $error['commentError'] = "Field is required!";
+        $isValid = false;
+    }
+
+    if ($isValid) {
+        $insertComment = "INSERT INTO comments (post_id, user_id, comment_text) VALUES ($getBlogId, $user_id, '$comment')";
+        $insertCommentResult = mysqli_query($conn, $insertComment);
+    }
+}
+
+// select comment
+$selectComment = "SELECT * FROM comments c INNER JOIN users u ON c.user_id = u.user_id WHERE post_id = '$getBlogId'";
+$selectCommentResult = mysqli_query($conn, $selectComment);
+
+$row =  mysqli_num_rows($selectCommentResult);
 ?>
 
 <link rel="stylesheet" href="bootstrap.min.css">
+
 <div class="row">
     <div class="col-12 col-lg-8">
         <p class="h1 fw-bold"><?php echo $data['title']; ?></p>
@@ -19,22 +52,65 @@ $data = mysqli_fetch_assoc($fetchResult);
         <p class="h3"><?php echo $data['content']; ?></p>
     </div>
     <div class="col-12 col-lg-4 shadow-lg">
-        <p class="fw-bolder h3">Comments</p>
+        <p class="fw-bolder h3"><?php echo ($row <= 1) ? "Comment ($row)" : "Comments ($row)"; ?></p>
         <div class="card shadow p-2">
             <div>
                 <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
                     alt=""
                     class="image">
-                <span><a href="viewOthersProfile.php" class="text-decoration-none text-dark">Rakesh</a></span>
+                <span><a href="viewOthersProfile.php" class="text-decoration-none text-dark fw-bold"><?php echo $userData['username'] ?></a></span>
             </div>
             <form action="" method="post">
                 <div class="mt-2">
-                    <textarea name="" id="" rows="2" class="form-control" placeholder="Write comment here!"></textarea>
+                    <textarea name="comment-text" id="" rows="2" class="form-control" placeholder="Write comment here!"></textarea>
+                    <span><?php echo isset($error['commentError']) ?  $error['commentError'] : ''; ?></span>
                 </div>
                 <div class="mt-2">
-                    <button class="bg-info text-white fw-bolder border-0 px-2 py-1 rounded-1">Comment</button>
+                    <input type="submit" class="btn btn-success fw-bolder border-0 px-2 py-1 rounded-1" value="Comment" name="submit">
                 </div>
             </form>
+        </div>
+        <div>
+            <div class="mt-4 h5 text-danger">
+                Dropdown Sort
+                <hr>
+            </div>
+            <div>
+
+                <?php
+                while ($commentData = mysqli_fetch_assoc($selectCommentResult)) {
+                ?>
+                    <div class="d-flex gap-2">
+                        <div>
+                            <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png"
+                                alt=""
+                                class="image">
+                        </div>
+                        <div>
+                            <div>
+                                <span><a href="viewOthersProfile.php" class="text-decoration-none text-dark fw-bold"><?php echo $commentData['username'] ?></a></span>
+                            </div>
+                            <div class="mt-0">
+                                <p><?php echo $commentData['created_at']; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p><?php echo $commentData['comment_text']; ?></p>
+                        <div class="d-flex justify-content-between">
+                            <div class="d-flex gap-4">
+                                <div>0 👍</div>
+                                <div>0 💬</div>
+                            </div>
+                            <div>Reply</div>
+                        </div>
+                        <hr>
+                    </div>
+
+                <?php
+                } ?>
+
+            </div>
         </div>
     </div>
 </div>
